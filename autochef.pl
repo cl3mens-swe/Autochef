c5adb4cb6c87b48e01d3a532bdc11631f40d44a9
Mikael Svantesson <mikael@distopic.net>
2011-10-24 16:26:25 +0200

    Pretty formatting.

--- autochef.pl
+++ autochef.pl
@@ -2,8 +2,70 @@
 
 # Written by felhan 23/3-2011, updated when needed
 
-my @verb = ("Konsolidera", "Harmonisera", "Virtualisera", "Outsourca", "Uppdatera", "Abstrahera", "Brainstorma kring", "Implementera", "Validera", "Benchmarka", "Standardisera", "Centralisera", "Decentralisera", "Streamlinea", "Verifiera", "Aktualisera", "Dokumentera", "Mindmappa");
-my @subj = ("changeprocessen", "ITIL", "kaffebryggaren", "SAP", "SCRUM", "agile development", "Jboss", "molnet", "SaaS", "SOA", "NoSQL", "sharepoint", "lean IT", "apparna", "ASP.NET", "grön IT", "tjänstestrukurerna", "MBWA", "clean desk", "tjänsteportföljen", "en RFC", "grejerna i pipen", "turnkey-solutions", "backlogen", "CRM", "polopoly", "disaster recovery-planen", "middleware", "Web 2.0-tjänsterna", "HTML5", "CMS");
+my @verb = qw(
+	Konsolidera
+	Harmonisera
+	Virtualisera
+	Outsourca
+	Uppdatera
+	Abstrahera
+	Implementera
+	Validera
+	Benchmarka
+	Standardisera
+	Centralisera
+	Decentralisera
+	Streamlinea
+	Verifiera
+	Aktualisera
+	Dokumentera
+	Mindmappa
+);
+
+@verb = (
+	@verb,
+
+	"Brainstorma kring"
+);
+
+my @subj = qw(
+	changeprocessen
+	ITIL
+	kaffebryggaren
+	SAP
+	SCRUM
+	Jboss
+	molnet
+	SaaS
+	SOA
+	NoSQL
+	sharepoint
+	lean IT
+	apparna
+	ASP.NET
+	grön IT
+	tjänstestrukurerna
+	MBWA
+	clean desk
+	tjänsteportföljen
+	en RFC
+	grejerna i pipen
+	turnkey-solutions
+	backlogen
+	CRM
+	polopoly
+	middleware
+	Web 2.0-tjänsterna
+	HTML5
+	CMS
+);
+
+@subj = (
+	@subj,
+
+	"disaster recovery-planen"
+	"agile development"
+);
 
 print "$verb[int(rand(@verb))] $subj[int(rand(@subj))]\n";
 
